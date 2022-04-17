f63b3a64f207ace111c6980c70de6a3c8229debc
Karina <puentek@oregonstate.edu>
2022-04-17 14:57:10 -0700

    adding a file for practice

---
+++ urdf/hand4.py
@@ -0,0 +1,15 @@
+import os
+import pybullet as p
+import time
+import pybullet_data
+from pybullet_object_models import ycb_objects
+physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+plane = p.loadURDF("data/plane.urdf")
+p.setGravity(0,0,-10)
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+urdfFlags = p.URDF_USE_SELF_COLLISION
+
+
+
+
+# git hub token: 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
